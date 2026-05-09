f4369388ee8240201c5ea8a76f23341c630a73f7
unknown <farhanabdillah328@gmail.com>
2026-05-09 10:57:06 +0700

    Fix: Menambahkan /api ke baseURL axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://vercel-test-gamma-three-41.vercel.app/',
+  baseURL: 'https://vercel-test-gamma-three-41.vercel.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
