95d155b0f69132600d06a3de532cfa3489ec7c0e
unknown <farhanabdillah328@gmail.com>
2026-05-09 09:48:20 +0700

    Update baseURL API ke Vercel backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://vercel-test-gamma-three-41.vercel.app/',
   headers: { 'Content-Type': 'application/json' },
 });
 
